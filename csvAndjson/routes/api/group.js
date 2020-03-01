fd9af62cefaf40a36bd4ba5b1a0b6961f5f426bc
devAon <yewon9742@gmail.com>
2020-03-01 17:50:48 +0900

    [REFACTORING] create csvManager module

--- csvAndjson/routes/api/group.js
+++ csvAndjson/routes/api/group.js
@@ -1,32 +1,25 @@
-var express = require('express');
-var router = express.Router();
-
-const csvtojson = require('csvtojson');
-const path = require('path');
-const json2csv = require('json2csv');
-const fs = require('fs');
+const express = require('express');
+const router = express.Router();
+const csvManager = require('../../module/csvManager')
 
 
 const filePath = './public/csvs/';
-const groupMap = {};
+
 
 router.get('/', async (req, res) => {
     try {
-        csvtojson().fromFile(path.join(filePath, 'group.csv')).then((groupArr) => {
-            //console.log(groupArr);
+        const groupArr = await csvManager.read('group.csv')
+        console.log(groupArr);
 
-
-            groupArr.forEach(element => {
-                groupMap[element.groupIdx] = element.name;
-            })
-            //console.log(groupMap);
+        const groupMap = {};
+        groupArr.forEach(element => {
+            groupMap[element.groupIdx] = element.name;
         })
+        //console.log(groupMap);
 
+        const memberArr = await csvManager.read('member.csv');
+        res.status(200).send(memberArr.map(it => `${it.name} : ${groupMap[it.groupIdx]}`).join(", "));
 
-        csvtojson().fromFile(path.join(filePath, 'member.csv')).then((memberArr) => {
-            //console.log(memberArr);
-            res.status(200).send(memberArr.map(it => `${it.name} : ${groupMap[it.groupIdx]}`).join(", "));
-        })
     } catch (err) {
         console.log(err);
         res.status(500).send('Internal Server Error');
@@ -39,33 +32,17 @@ router.get('/', async (req, res) => {
 
 router.get('/mixer', async (req, res) => {
     try {
+        const memberArr = await csvManager.read('member.csv');
+        var mixerArr = [...memberArr];
+        mixerArr.forEach(element => {
+            const mixIdx = parseInt(Math.random() * memberArr.length - 1);
+            const tempGroupIdx = element.groupIdx;
+            element.groupIdx = mixerArr[mixIdx].groupIdx;
+            mixerArr[mixIdx].groupIdx = tempGroupIdx;
+        });
 
-        csvtojson().fromFile(path.join(filePath, 'member.csv')).then((memberArr) => {
-
-            var mixerArr = [...memberArr];
-            mixerArr.forEach(element => {
-                const mixIdx = parseInt(Math.random() * memberArr.length - 1);
-
-                const tempGroupIdx = element.groupIdx;
-                element.groupIdx = mixerArr[mixIdx].groupIdx;
-                mixerArr[mixIdx].groupIdx = tempGroupIdx;
-            })
-
-
-            const memberFilePath = './routes/api/member';
-            const resultCsv = json2csv.parse(mixerArr);
-
-            fs.writeFileSync(`${memberFilePath}.csv`, resultCsv, function (err) {
-                if (err) {
-                    console.log("err");
-                    return;
-                }
-                res.status(200).send('success');
-            });
-
-        })
+        await csvManager.write('member.csv', mixerArr)
         res.status(200).send('success');
-
     } catch (err) {
         console.log(err);
         res.status(500).send('Internal Server Error');
@@ -79,13 +56,11 @@ router.get('/:groupIdx', async (req, res) => {
     const { groupIdx } = req.params;
 
     try {
-        csvtojson().fromFile(path.join(filePath, 'member.csv')).then((memberArr) => {
-            //console.log(memberArr);
-            res.status(200).send(memberArr
-                .filter(it => it.groupIdx === groupIdx)
-                .map(it => `${it.name}`)
-                .join(', '));
-        })
+        const memberArr = await csvManager.read('member.csv');
+        res.status(200).send(memberArr
+            .filter(it => it.groupIdx === groupIdx)
+            .map(it => `${it.name}`)
+            .join(', '));
     } catch (err) {
         console.log(err);
         res.status(500).send('Internal Server Error');
